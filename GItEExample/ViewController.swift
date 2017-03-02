6c01e6a3b777b3e2b850b754462077e2cdbe2cb5
sreenivasulu <sannala@pnl-group.com>
2017-03-02 15:09:36 +0800

    Initial Commit

---
+++ GItEExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GItEExample
+//
+//  Created by emedia on 2/3/17.
+//  Copyright © 2017 emedia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
